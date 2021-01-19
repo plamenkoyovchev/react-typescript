0262c02ac3f60daca05ba61ba92a7a673cb8a0e9
plamenkoyovchev <plamenkooo@gmail.com>
2021-01-20 01:07:46 +0200

    Add function in props

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,12 +1,23 @@
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}></button>
+    </div>
+  );
 };
 
 // preferred way!
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}></button>
+    </div>
+  );
 };
