c1994f5094658604599965e87bc2809650669557
plamenkoyovchev <plamenkooo@gmail.com>
2021-01-22 21:20:46 +0200

    Clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hi!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hi!</h1>
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
