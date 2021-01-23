c78fc695b3e42b155af71eda8811f2dfd27c97f0
plamenkoyovchev <plamenkooo@gmail.com>
2021-01-23 12:49:02 +0200

    add repositories reducer

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,42 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface RepositoriesState {
+    data: string[];
+    error: string;
+    loading: boolean;
+}
+
+const initialState: RepositoriesState = {
+    data: [],
+    error: '',
+    loading: false
+};
+
+export const reducer = (state: RepositoriesState = initialState, action: Action) => {
+    switch (action.type) {
+        case ActionType.SEARCH_REPOSITORIES:
+            return {
+                ...state,
+                data: [],
+                loading: true,
+                error: ''
+            };
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+            return {
+                ...state,
+                data: action.payload,
+                error: '',
+                loading: false
+            };
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
+            return {
+                ...state,
+                data: [],
+                error: action.payload,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
