bc6883456ecf29684bdc62fa82ad470827eb1a8e
plamenkoyovchev <plamenkooo@gmail.com>
2021-01-23 14:26:18 +0200

    RepositoriesList component basic implementation

--- src/components/RepositoriesList/RepositoriesList.tsx
+++ src/components/RepositoriesList/RepositoriesList.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
 
-const RepositoriesList = () => {
-  return <div></div>;
+const RepositoriesList: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { searchRepositories } = useActions();
+
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    searchRepositories(searchTerm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" value={searchTerm} onChange={onChangeHandler} />
+        <button type="submit">Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
+    </div>
+  );
 };
 
 export default RepositoriesList;
